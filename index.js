5a7f30dae684b0efc41c13f388390e8f8b68c433
Mary Schmidt <maryschmidt@MacBook-Pro.local>
2020-08-01 15:18:48 -0500

    updated all files

---
+++ index.js
@@ -0,0 +1,115 @@
+function startQuizAgain() {
+  $("body").on("click", "#restart", (event) => {
+    generateQuestion();
+  });
+}
+
+function submitAnswer() {
+  $("body").on("submit", "#js-questions", function (event) {
+    event.preventDefault();
+    let questionNumber = STORE.questions[STORE.questionNumber];
+    let selectedOption = $("input[name=options]:checked").val();
+    if (!selectedOption) {
+      alert("Choose an option");
+      return;
+    }
+    let id_num = questionNumber.options.findIndex((i) => i === selectedOption);
+    let id = "#js-r" + ++id_num;
+    $("span").removeClass("right-answer wrong-answer");
+    if (selectedOption === questionNumber.answer) {
+      STORE.score++;
+      $(`${id}`).append(
+        `You got it right<br/> The answer is "${questionNumber.answer}"<br/>`
+      );
+      $(`${id}`).addClass("right-answer");
+    } else {
+      $(`${id}`).append(
+        `You got it wrong<br/> The answer is "${questionNumber.answer}"<br/>`
+      );
+      $(`${id}`).addClass("wrong-answer");
+    }
+    $(`${id}`).append(questionNumber.explanation);
+
+    STORE.questionNumber++;
+    $("#js-score").text(`Score: ${STORE.score}/${STORE.questions.length}`);
+    $("#answer").hide();
+    $("input[type=radio]").attr("disabled", true);
+    $("#next-question").show();
+  });
+}
+
+function displayResults() {
+  let resultHtml = $(
+    `<div class="results">
+        <form id="js-restart-quiz">
+        <legend>Your Score is: ${STORE.score}/${STORE.questions.length}</legend>
+        </div>
+        <div>
+        <button type="submit" id="restart">Restart Quiz</button>
+        </div>`
+  );
+  STORE.questionNumber = 0;
+  STORE.score = 0;
+  $("#main-container").html(resultHtml);
+}
+
+function handleQuestions() {
+  $("body").on("click", "#next-question", (event) => {
+    STORE.questionNumber === STORE.questions.length
+      ? displayResults()
+      : generateQuestion();
+  });
+}
+
+function updateQuestionAndScore() {
+  const html = $(`<ul> <li id="js-answered">Question Number: ${
+    STORE.questionNumber + 1
+  }/${STORE.questions.length}
+    </li>
+    <li id="js-score">Score: ${STORE.score}</li>
+    </ul>`);
+  $("#question-number-and-score").html(html);
+}
+
+function updateOptions() {
+  let question = STORE.questions[STORE.questionNumber];
+  for (let i = 0; i < question.options.length; i++) {
+    $(".js-options").append(`<input type='radio' name='options' 
+    id='options${i + 1}' value="${question.options[i]}" tabindex="${i + 1}">
+    <label for="options${i + 1}"> ${question.options[i]}</label><br/>
+    <span id="js-r${i + 1}"></span>`);
+  }
+}
+
+function generateQuestion() {
+  updateQuestionAndScore();
+  let questionObj = STORE.questions[STORE.questionNumber];
+  const questionHtml = $(`
+    <div>
+    <form id="js-questions">
+    <h3>${questionObj.question}</h3>
+    <div class="js-options">
+    </div>
+    <button type="submit" id="answer">Submit</button>
+    <button type="submit" id="next-question">Next</button>
+  </form>
+    </div>
+`);
+  $("#main-container").html(questionHtml);
+  updateOptions();
+  $("#next-question").hide();
+}
+
+function startQuiz() {
+  $("#start").on("click", function (event) {
+    generateQuestion();
+  });
+}
+
+function quizApp() {
+  startQuiz();
+  handleQuestions();
+  submitAnswer();
+  startQuizAgain();
+}
+$(quizApp);
---
+++ store.js
@@ -0,0 +1,88 @@
+const STORE = {
+  questions: [
+    //1
+    {
+      question: "How many US Presidents were slave owners?",
+      options: ["12", "8", "3", "13"],
+      answer: "12",
+      explanation:
+        "Washington, Jefferson, Madison, Monroe, Jackson, VanBuren, Polk, Tyler, A. Johnson, Grant, Harrison, and Taylor",
+    },
+    //2
+    {
+      question:
+        "When were slaves first brought to the English colonies in North America?",
+      options: ["1582", "1490", "1619", "1750"],
+      answer: "1619",
+      explanation: "in Jamestown Virgina",
+    },
+    //3
+    {
+      question:
+        "The 1860 US Census estimated there were this many enslaved people that year?",
+      options: ["3 million", "4 million", "6 million", "8 million"],
+      answer: "4 million",
+      explanation: "an estimated 14% of the US population",
+    },
+    //4
+    {
+      question: "When did African-American men first have the right to vote?",
+      options: ["1865", "1900", "1870", "1920"],
+      answer: "1870",
+      explanation: "the 15th Amendment",
+    },
+    //5
+    {
+      question: "When did African-American women first have the right to vote?",
+      options: ["1965", "1920", "1890", "1900"],
+      answer: "1965",
+      explanation:
+        "with the Voting Rights Act signed by President Lyndon Johnson",
+    },
+    //6
+    {
+      question: "When did white women first have the right to vote?",
+      options: ["1965", "1920", "1860", "1900"],
+      answer: "1920",
+      explanation: "the 19th Amendement",
+    },
+    //7
+    {
+      question: "How many years was the Confederate United States a country?",
+      options: ["6", "9", "2", "4"],
+      answer: "4",
+      explanation: "1861-1865, the same years as the American Civil War",
+    },
+    //8
+    {
+      question: "When was the first African-American elected to US Congress?",
+      options: ["1930", "1870", "1890", "1900"],
+      answer: "1870",
+      explanation:
+        "two men were elected to serve in Congress, Senator Hiram Revels of Mississippi, and Representative Joseph Rainey of South Carolina",
+    },
+    //9
+    {
+      question: "How many members of US Congress are African-American in 2020?",
+      options: ["27", "93", "58", "114"],
+      answer: "58",
+      explanation: "2 Senators and 56 Representatives",
+    },
+    //10
+    {
+      question:
+        "Who was the first woman to run for the President of the United States?",
+      options: [
+        "Hilary Clinton",
+        "Shirley Chislom",
+        "Geraldiine Farraro",
+        "Carole Mosley Braun",
+      ],
+      answer: "Shirley Chislom",
+      explanation:
+        "in 1972, she was the first African-American woman elected to the US Congress in 1968",
+    },
+  ],
+  questionNumber: 0,
+  score: 0,
+};
